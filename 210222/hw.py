958bd267a0f8683a64445cf02dc97f665e604569
vldln-biven'мамы.net <strifee322@gmail.com>
2021-02-28 22:50:38 +0500

    homework(осветление изображения)

---
+++ 210222/hw.py
@@ -0,0 +1,15 @@
+import numpy as np
+from PIL import Image
+
+image_name = 'kit.jpg'
+image = Image.open(image_name)
+
+image_np = np.array(image)
+print(image_np.size, image_np.shape)
+
+image_np_conv = image_np
+image_np_conv[:, :, :] += 25
+
+new_image = Image.fromarray(image_np_conv.astype('uint8'))
+save_name = 'kitlightt.jpg'
+new_image.save(save_name)
